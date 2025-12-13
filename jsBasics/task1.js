238b2f9e5891547ba2630c5962704f5e7acce9b1
Inna Onosovska <inna_polyak@ukr.net>
2025-12-14 00:55:39 +0200

    Add JS basics homework 4.1

---
+++ jsBasics/task1.js
@@ -0,0 +1,17 @@
+/*Завдання 1: Оголошення змінних для примітивних типів
+
+Створіть програму, в якій ви оголосите змінні для кожного з 
+примітивних типів даних: рядок, число, буль, null та undefined. 
+Присвойте їм значення та виведіть кожну змінну на консоль.*/
+
+const myString = "Hello, JavaScript!";
+const myNumber = 12; 
+const myBoolean = true;
+const myNull = null;
+let myUndefined; 
+
+console.log("String:", myString);
+console.log("Number:", myNumber);
+console.log("Boolean:", myBoolean);
+console.log("Null:", myNull);
+console.log("Undefined:", myUndefined);
